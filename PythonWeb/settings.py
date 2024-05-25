d9659c97221a1acb06851e47088e2cd0a43c5eed
PhuongTran <phuong126pt@gmail.com>
2024-05-26 00:05:42 +0700

    Second

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-3=z08z!b%bxclqhksn$)8ekm0wb%wu(j616w7_j9j%e7b-j!_s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', ' ']
+ALLOWED_HOSTS = ['.vercel.app']
 # ALLOWED_HOSTS = []
 
 # Application definition
